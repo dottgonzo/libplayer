bd1c791d5596a7bf10940976c8dcf56267db96fb
dario <darioyzf@gmail.com>
2016-06-20 10:26:12 +0200

    starting

---
+++ index.ts
@@ -0,0 +1,8 @@
+let play = function (obj: { dom: { id: string }, remote: { domain: string, channel: string, token: string } }) {
+
+    let domnode = document.getElementById(obj.dom.id);
+
+
+    domnode.innerHTML = "play!"
+
+}
